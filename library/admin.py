9ac372ebb9243aa25e3e9559fece36cd0e01fe11
Rosana Rufer <rosanarufer@gmail.com>
2024-06-18 20:58:24 +0100

    uses TabularInline

--- library/admin.py
+++ library/admin.py
@@ -2,17 +2,27 @@ from django.contrib import admin
 
 from library.models import Author, AuthorsBooks, Book
 
+
+class AuthorsBooksInline(admin.TabularInline):
+    model = AuthorsBooks
+    extra = 1
+    fields = ['author', 'book']
+    
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     # This will raise an error
     #     ERRORS:
     # <class 'library.admin.BookAdmin'>: (admin.E013) The value of 'fields' cannot include the ManyToManyField 'authors', because that field manually specifies a relationship model.
-    fields = ['name', 'authors'] 
+    fields = ['name']
+    list_display = ['name', 'get_authors'] 
+    inlines = [AuthorsBooksInline]
+    def get_authors(self, obj):
+        return ", ".join([author.name for author in obj.authors.all()])
+    get_authors.short_description = 'Authors'
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     fields = ['name']
+    list_display = ['name'] 
+    inlines = [AuthorsBooksInline]
 
-@admin.register(AuthorsBooks)    
-class AuthorsBooksAdmin(admin.ModelAdmin):
-    fields = ['author', 'book']
--- library/models.py
+++ library/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     name = models.CharField(max_length=100)
-    authors = models.ManyToManyField(Author, blank=True)
+    authors = models.ManyToManyField(Author, blank=True, through='AuthorsBooks')
+    def __str__(self):
+        return f"{self.name} by {', '.join([author.name for author in self.authors.all()])}"
 
 class AuthorsBooks(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
